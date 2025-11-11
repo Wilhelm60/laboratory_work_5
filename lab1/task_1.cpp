017b22a18d61c26dfd82044d7097b10a495dcc8b
Wilhelm60 <63043331+Wilhelm60@users.noreply.github.com>
2025-11-11 12:35:29 +0300

    Create task_1.cpp

---
+++ lab1/task_1.cpp
@@ -0,0 +1,35 @@
+include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Введите количество элементов массива: ";
+    cin >> n;
+
+    int* arr = new int[n];
+
+    cout << "Введите " << n << " элементов: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Массив: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+
+    cout << "Адрес начала массива: " << arr << "\n\n";
+
+    for (int i = 0; i < n; i++) {
+        cout << "Элемент arr[" << i << "] = " << arr[i]
+             << " | Адрес: " << &arr[i]
+             << " | Смещение от начала = " << (char*)&arr[i] - (char*)arr
+             << " байт\n";
+    }
+
+    delete[] arr;
+    return 0;
+}
